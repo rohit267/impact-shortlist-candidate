66d14ff3c5f21f15e235176f8dea3b16f06b34a4
Rohit Mahto <rohitmahto267@gmail.com>
2021-01-01 20:55:21 +0530

    checking api

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,13 +1,37 @@
-import React from 'react';
-import {useState,useEffect} from 'react';
-import Navbar from '../components/Navbar';
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Navbar from "../components/Navbar";
 
 const Homepage = () => {
-    return (
-        <div>
-            <Navbar/>
-        </div>
-    );
+  useEffect(() => {
+    let config = {
+      method: "get",
+      url:
+        "https://s3-ap-southeast-1.amazonaws.com/he-public-data/users49b8675.json",
+      headers: {},
+    };
+
+    axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+    fetch(
+      "https://s3-ap-southeast-1.amazonaws.com/he-public-data/users49b8675.json"
+    )
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 };
 
 export default Homepage;
